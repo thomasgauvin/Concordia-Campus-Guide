835a2e289da9ca4aea390acb41d7390ef94c3b80
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2020-03-01 12:34:10 -0500

    #38: fully merged with master, ready to work on business logic

--- app/src/main/java/com/example/concordia_campus_guide/Activities/RoutesActivity.java
+++ app/src/main/java/com/example/concordia_campus_guide/Activities/RoutesActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.concordia_campus_guide.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class RoutesActivity extends AppCompatActivity {
 
